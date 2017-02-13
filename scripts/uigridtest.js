27916ced1e5f876aa4174791ef22b8e32eca8749
Atsushi Hayashi <hycats@gmail.com>
2017-02-14 04:08:57 +0900

    font, cellFilter

--- scripts/uigridtest.js
+++ scripts/uigridtest.js
@@ -7,7 +7,8 @@ mymodule
             enableSorting: false,
             columnDefs: [
                 {
-                    field: 'sdate', displayName: '日付', width: 80
+                    field: 'sdate', displayName: '日付', width: 100,
+                    cellFilter: 'date: "yyyy/MM/dd"', cellClass: 'grid-date'
                 },
                 {
                     field: 'fname', displayName: '費目', width: '10%'
@@ -22,16 +23,33 @@ mymodule
                     field: 'check', displayName: '済', type: 'boolean', width: 35
                 },
                 {
-                    field: 'income', displayName: '収入', width: '10%'
+                    field: 'income', displayName: '収入', width: '12%',
+                    cellFilter: 'currency: "":0', cellClass:'grid-numbers'
                 },
                 {
-                    field: 'outgo', displayName: '支出', width: '10%'
+                    field: 'outgo', displayName: '支出', width: '12%',
+                    cellFilter: 'currency: "":0', cellClass:'grid-numbers'
                 },
                 {
-                    field: 'balance', displayName: '残高', width: '10%'
+                    field: 'balance', displayName: '残高', width: '12%',
+                    cellFilter: 'currency: "":0', cellClass:'grid-numbers'
                 },
                 {
                     field: 'comment', displayName: '備考'
+                }
+            ],
+            data: [
+                {
+                    sdate: new Date(2017,01,10),
+                    fname:'食費',
+                    lname:'その他',
+                    product:'その他',
+                    outgo: 5800,
+                    balance: 120000000
+                },
+                {
+                    sdate: new Date(2017,01,13),
+                    outgo: 12300
                 }
             ]
         };
